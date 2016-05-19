0d788a66f897f7445523f101629764b520af0289
Dennis Ting <dennisting@gmail.com>
2016-05-19 06:54:06 -0700

    Reorder visualizations on home page

--- components/content/Content.js
+++ components/content/Content.js
@@ -4,13 +4,13 @@ import { Layout, SortVisualization, MergeSortVisualization } from '~/components'
 function Content({ arr }) {
   return (
     <Layout>
+      <SortVisualization type="bogosort" title="Bogo Sort" arr={arr} />
       <SortVisualization type="bubblesort" title="Bubble Sort" arr={arr} />
       <SortVisualization type="selectionsort" title="Selection Sort" arr={arr} />
       <SortVisualization type="insertionsort" title="Insertion Sort" arr={arr} />
-      <SortVisualization type="quicksort" title="Quick Sort" arr={arr} />
       <SortVisualization type="heapsort" title="Heap Sort" arr={arr} />
+      <SortVisualization type="quicksort" title="Quick Sort" arr={arr} />
       <SortVisualization type="mergesort" title="Merge Sort" arr={arr} aux={Array(100).fill(99)} />
-      <SortVisualization type="bogosort" title="Bogo Sort" arr={arr} />
     </Layout>
   );
 }
